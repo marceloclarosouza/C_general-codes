c72c5ecb23b4a97150469dda94ff8541670eb1fd
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-03-25 09:20:12 -0300

    Update exe4FATEC.c

--- exe4FATEC.c
+++ exe4FATEC.c
@@ -5,21 +5,49 @@ delta, o X1 e o X2. (utilize retorno vazio e passe os dados por referência)*/
 #include<stdlib.h>
 #include<math.h>
 
-void raizes (float a, float b, float c)
+
+void *Delta (float a, float b, float c)
+{
+    float delta;
+    delta = (b*b) - 4*a*c;
+    if (delta < 0)
+        printf("Delta negativo.\nEsta equacao nao possui raizes reais\n");
+}
+
+void *raizes (float a, float b, float c)
 {
     float delta, x1, x2;
 
     delta = (b*b) -4*a*c;
 
-    if (delta <0)
-        printf("Delta negativo.\nEsta equacao nao possui raizes reais\n")
-    else if (delta == 0)
+    if (delta == 0)
     {
-        x = -b/(2*a);
+        x1 = -b/(2*a);
+        x2 = -b/(2*a);
     }
-    else
+    else if (delta > 0)
     {
         x1 = ((-b) + sqrt(delta(a,b,c))/2*a);
-        x2 = ((-b) - sqrt(delta(a,b,c)/2*a));
+        x2 = ((-b) - sqrt(delta(a,b,c))/2*a);
     }
+}
+
+int main ()
+{
+    float a, b, c, raiz, delta;
+    system("cls");
+    printf("Digite o valor de 'a'\n");
+    scanf("%f", &a);
+    printf("Digite o valor de 'b'\n");
+    scanf("%f", &b);
+    printf("Digite o valor de 'c'\n");
+    scanf("%f", &c);
+
+    delta = &Delta(a,b,c);
+    printf("Delta = %.1f", delta);
+
+    raiz = &raizes(a,b,c);
+    printf("Os valores de x1 e x2 sao %f", raiz);
+    system("pause");
+    return 0;
 }
