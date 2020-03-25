358571f8775a5b6f1a2e77d8329d0b36ad15fe05
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-03-25 16:49:39 -0300

    Update exe6FATEC.c

--- exe6FATEC.c
+++ exe6FATEC.c
@@ -15,25 +15,82 @@ void cabecalho (){
 }
 
 void push (int *vet)
+{
+    int fim = -1;
+    if (fim < (MAX -1){
+        fim++;
+        printf("Digite o numero que deseja adicionar\n");
+        scanf("%d", &vet[fim]);
+    }
+    else
+    {
+        printf("Impossivel inserir!\nVetor cheio!");
+    }
+}
 
 void pop (int *vet)
+{   
+    int fim;
 
-void pmostra (int *vet)
+    if (fim >= 0){
+        fim--;
+        printf("Removido com sucesso!");
+    }
+    else
+    {
+        printf("Impossivel remover.\nVetor vazio!!!\n");
+    }
+}
+
+void mostra (int *vet)
+{
+    int i;
+
+    if (fim >=0){
+        for (i = 0; i <= fim; i++)
+            printf("%d\t", vet[i]);
+    }
+    else
+    {
+        printf("Vetor vazio!!!\n");
+    }    
+
+}
 
 int main ()
 {
-    int vet[MAX], fim = -1, i;
+    int vet[MAX], fim = -1, i, op;
 
-do {
-    system("cls");
-    cabecalho()
+    do {
+        system("cls");
+        cabecalho()
+        printf("\n1 - Inserir");
+        printf("\n2 - Retirar");
+        printf("\n3 - Mostrar");
+        printf("\n9 - Sair");
+        scanf("%d", &op);
 
+            switch (op){
 
+                case 1: push(&vet);
+                        printf("Adicionado com sucesso\n")
+                        system ("pause");
+                        break;
 
+                case 2: pop(&vet);
+                        printf("Removido com sucesso\n");
+                        system("pause");
+                        break;
 
+                case 3: mostra(&vet);
+                        printf("%d", vet);
+                        system("pause");
+                        break;
 
+ 
+            }
 
-} while(op!=9);
+    } while(op!=9);
 
     return 0;
 }
