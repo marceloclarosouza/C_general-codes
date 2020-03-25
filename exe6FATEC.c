90270bebcd23953684835645f054065dbb20ab19
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-03-25 15:59:46 -0300

    under construction

---
+++ exe6FATEC.c
@@ -0,0 +1,39 @@
+/*Escreva um programa que armazene números inteiros em uma pilha estática. Você
+deverá elaborar um menu com as opções: insere (push), remove (pop) e mostra pilha.
+Cada opção do menu deverá ser implementado em uma função específica para isso.
+(você poderá utilizar o programa implementado em sala e alterá-lo para adicionar as
+funções solicitadas neste exercício.).*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 10
+
+void cabecalho (){
+    printf("************************************************\n");
+    printf("                    M E N U                     \n");
+    printf("************************************************\n");
+}
+
+void push (int *vet)
+
+void pop (int *vet)
+
+void pmostra (int *vet)
+
+int main ()
+{
+    int vet[MAX], fim = -1, i;
+
+do {
+    system("cls");
+    cabecalho()
+
+
+
+
+
+
+} while(op!=9);
+
+    return 0;
+}
